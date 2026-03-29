cf250a959c8cd213e74d70fd98af8c4620702872
Christoph Maser <christoph.maser@1und1.de>
2026-03-29 12:27:53 +0200

    chore: tune resource limits for runner set

--- uninstall.sh
+++ uninstall.sh
@@ -6,8 +6,6 @@ kubectl config get-contexts openvox || exit 1
 
 
 NAMESPACE="arc-system"
-CONTROLLER_RELEASENAME="arc"
-
 
 helm uninstall  runnerset-voxpupuli \
     --namespace "$NAMESPACE" \
@@ -17,7 +15,3 @@ helm uninstall runnerset-openvoxproject \
     --namespace "$NAMESPACE" \
     --wait
 
-
-helm uninstall "$CONTROLLER_RELEASENAME" \
-    --namespace "$NAMESPACE" \
-    --wait
